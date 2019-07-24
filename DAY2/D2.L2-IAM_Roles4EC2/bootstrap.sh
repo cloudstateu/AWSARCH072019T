179b8df9c561710b9166ea5ece3f5339398805a5
mrdoro <mrdoro@me.com>
2019-07-24 11:22:37 +0200

    v1.0

--- DAY2/D2.L2-IAM_Roles4EC2/bootstrap.sh
+++ DAY2/D2.L2-IAM_Roles4EC2/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yum -y install httpd php\n
-chkconfig httpd on\n
-/etc/init.d/httpd start\n
-aws s3 cp s3://csa03-studentX/index.php /var/www/html\n
+yum -y install httpd php \n
+chkconfig httpd on \n
+/etc/init.d/httpd start \n
+aws s3 cp s3://csa03-studentX/index.php /var/www/html \n
